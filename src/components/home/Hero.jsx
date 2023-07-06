a9d3fbf1373141e6922daf39c9cdfdde0a28fedc
Henrique Lira Da Silva <henrique.2019210942@unicap.br>
2023-07-05 22:09:20 -0300

    page/Home

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { home } from "../data/dummydata";
+import TypewriterComponent from "typewriter-effect";
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+      {home.map((val, i) => (
+        <div className="heroContainer">
+          <h3>{val.text}</h3>
+          <h1>
+            <TypewriterComponent
+              options={{
+                strings: [`${val.name}`, `${val.post}`, `${val.design}`],
+                autoStart: true,
+              }}
+            />
+          </h1>
+          <p>{val.desc}</p>
+          <button className="primaryBtn">Download CV</button>
+        </div>
+      ))}
+    </section>
+  );
+};
